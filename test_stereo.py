3e609d98d915d27b3bae3b84906954b8c0ac6f12
Tvisha <shahtvisha03@gmail.com>
2026-07-06 20:46:22 -0700

    test: add Rerun visualization + remove debug handler

--- test_stereo.py
+++ test_stereo.py
@@ -1,6 +1,8 @@
 from dimos.hardware.sensors.camera.realsense.camera import RealSenseCamera
 from dimos.perception.stereo_point_cloud import StereoPointCloud
 from dimos.mapping.costmapper import CostMapper
+from dimos.visualization.rerun.bridge import RerunBridgeModule
+from dimos.visualization.rerun.websocket_server import RerunWebSocketServer
 from dimos.core.coordination.blueprints import autoconnect
 from dimos.core.coordination.module_coordinator import ModuleCoordinator
 
@@ -11,8 +13,10 @@ if __name__ == '__main__':
             RealSenseCamera.blueprint(enable_depth=True, enable_pointcloud=False),
             StereoPointCloud.blueprint(),
             CostMapper.blueprint(),
+            RerunBridgeModule.blueprint(),
+            RerunWebSocketServer.blueprint(),
         )
-        .global_config(n_workers=4)
+        .global_config(n_workers=6)
     )
     coordinator = ModuleCoordinator.build(pipeline, {})
     coordinator.start()
